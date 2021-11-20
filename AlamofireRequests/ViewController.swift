f8cb6dca60866c2b73bd91a08aad13e7a1e3e9cc
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2021-11-20 09:44:00 -0500

    Initial Commit

---
+++ AlamofireRequests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireRequests
+//
+//  Created by Oscar David Myerston Vega on 20/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
